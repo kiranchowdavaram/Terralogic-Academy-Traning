ecf65833dbcba70c11399908644f634c8e0f4f59
SaiKiran <kiranchowdavaram@gmail.com>
2025-10-19 15:08:07 +0530

    commit

---
+++ my-typescript-project/src/classfirst.tsx
@@ -0,0 +1,11 @@
+import React ,{Component} from "react";
+class First extends Component {
+    render() {
+        return (
+            <div>
+                <h1>first Component</h1>
+            </div>
+        )
+    }
+}
+export default First;
---
+++ my-typescript-project/src/classsecond.tsx
@@ -0,0 +1,11 @@
+import React ,{Component} from "react";
+class Second extends Component {
+    render() {
+        return (
+            <div>
+                <h1>second Component</h1>
+            </div>
+        )
+    }
+}
+export default Second;
